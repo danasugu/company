1919415a41cbdd31cdbcab2cf0d780c4ce321779
Dana Sugu <dana.sugu@gmail.com>
2021-09-05 15:45:39 +0300

    Update quick-win.php

--- quick-win.php
+++ quick-win.php
@@ -9,6 +9,7 @@ $dsn = "mysql:host=$db_host; dbname=$db_name";
 
 $db_connection = new PDO($dsn, $db_username, $db_password);
 
+//storing all in $query
 $query = "SELECT * FROM user";
-
+//run that $query
 $results = $db_connection->query($query);
